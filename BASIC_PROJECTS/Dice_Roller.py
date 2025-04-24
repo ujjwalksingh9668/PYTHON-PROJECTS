d8085fd6c6e50e01ee28693c7431f3bc2939b6dc
UJJWAL KUMAR SINGH <ujjwalksingh25@gmail.com>
2025-04-24 21:47:26 +0530

    Create Dice_Roller.py

---
+++ BASIC_PROJECTS/Dice_Roller.py
@@ -0,0 +1,47 @@
+# Dice roller game....
+
+# Solution:-
+import random
+user = input("do yo want to roll the dice: ")
+while user == "yes":
+    k= random.randint(1, 6)
+    if k == 1:
+        print("--------")
+        print("|      |")
+        print("|   *  |")
+        print("|      |")
+        print("--------")
+    if k == 2:
+        print("--------")
+        print("|     *|")
+        print("|      |")
+        print("|*     |")
+        print("--------")
+    if k == 3:
+        print("--------")
+        print("|     *|")
+        print("|  *   |")
+        print("|*     |")
+        print("--------")
+    if k == 4:
+        print("--------")
+        print("|*    *|")
+        print("|      |")
+        print("|*    *|")
+        print("--------")
+    if k == 5:
+        print("--------")
+        print("|*    *|")
+        print("|   *  |")
+        print("|*    *|")
+        print("--------")
+    if k == 6:
+        print("--------")
+        print("|* * *|")
+        print("|* * *|")
+        print("|* * *|")
+        print("--------")
+    user = input("do you want to roll the dice again")
+
+
+
